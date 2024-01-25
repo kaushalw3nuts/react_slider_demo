f98cd75b4367ebd9158ace0773ede97e5b117798
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2024-01-25 19:21:35 +0530

    smooth scroll

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,27 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '@/styles/fonts.css';
 import '@/styles/globals.css';
 import '@/styles/responsive.css';
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import('locomotive-scroll')).default;
+      const locomoScroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+      });
+    };
+
+    loadLocomotiveScroll();
+  }, []);
+
+  
   return (
     <>
-      <Component {...pageProps} />
+      <main data-scroll-container>
+        <Component {...pageProps} />
+      </main>
     </>
   )
 }
--- src/pages/index2.js
+++ src/pages/index2.js
@@ -1,24 +1,10 @@
 import Head from "next/head";
 import HookuseState from "@/components/HookuseState";
 import Header from "@/components/Header";
-import LocomotiveScroll from "locomotive-scroll";
-import { useEffect } from "react";
 
 
 
 export default function Hook () {
-
-	useEffect(() => {
-		const Locoscroll = new LocomotiveScroll({
-			el: document.querySelector("[data-scroll-container]"),
-			smooth: true,
-		});
-		return () => {
-			if (Locoscroll) {
-				Locoscroll.destroy();
-			}
-		  };
-	}, []);
 
 	return (
 	  <>
